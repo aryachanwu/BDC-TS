56680565e19e94313db94e9853576512177d366c
wuchan <wuchan@inspur.com>
2023-06-12 04:04:53 +0000

    add last query

--- bulk_query_gen/timescaledb/timescale_vehicle_common.go
+++ bulk_query_gen/timescaledb/timescale_vehicle_common.go
@@ -45,6 +45,10 @@ func (d *TimescaleVehicle) AvergeValueOneMonthOneHost(q bulkQuerygen.Query) {
 	d.AvergeValueNHosts(q, 1, time.Hour*24*7*30)
 }
 
+func (d *TimescaleVehicle) LastValueOneHost(q bulkQuerygen.Query) {
+	d.LastValueNHosts(q, 1)
+}
+
 // AvergeValueNHosts
 // select avg(value5) from t.p.v where time_stamp >= start_time and time_stamp <= end_time
 func (d *TimescaleVehicle) AvergeValueNHosts(qi bulkQuerygen.Query, nhosts int, timeRange time.Duration) {
@@ -71,4 +75,32 @@ func (d *TimescaleVehicle) AvergeValueNHosts(qi bulkQuerygen.Query, nhosts int, 
 	valueId := rand.Intn(59) + 1
 	valueName := "value" + strconv.Itoa(valueId)
 	q.QuerySQL = []byte(fmt.Sprintf("select avg(%s) from vehicle and time >=%d and time < %d", valueName, interval.StartUnixNano(), interval.EndUnixNano()))
+}
+
+// LastValueNHosts
+// select timestamp, VIN, value4 from (select timestamp, VIN, value4, rank() over(order by timestamp desc range between unbounded preceding and unbounded following) as rank from accounts) t where rank=1
+func (d *TimescaleVehicle) LastValueNHosts(qi bulkQuerygen.Query, nhosts int) {
+	//interval := d.AllInterval.RandWindow(timeRange)
+	nn := rand.Perm(d.ScaleVar)[:nhosts]
+
+	hostnames := []string{}
+	for _, n := range nn {
+		hostnames = append(hostnames, fmt.Sprintf("host_%d", n))
+	}
+
+	hostnameClauses := []string{}
+	for _, s := range hostnames {
+		hostnameClauses = append(hostnameClauses, fmt.Sprintf("hostname = '%s'", s))
+	}
+
+	//combinedHostnameClause := strings.Join(hostnameClauses, " or ")
+
+	humanLabel := fmt.Sprintf("Timescale max cpu, rand %4d hosts", nhosts)
+
+	q := qi.(*SQLQuery)
+	q.HumanLabel = []byte(humanLabel)
+	q.HumanDescription = []byte(fmt.Sprintf("%s: %s", humanLabel))
+	valueId := rand.Intn(59) + 1
+	valueName := "value" + strconv.Itoa(valueId)
+	q.QuerySQL = []byte(fmt.Sprintf("select timestamp, VIN, value4 from (select timestamp, VIN, value4, rank() over(order by timestamp desc range between unbounded preceding and unbounded following) as rank from accounts) t where rank=1"))
 }
---
+++ bulk_query_gen/timescaledb/timescale_vehicle_last.go
@@ -0,0 +1,22 @@
+package timescaledb
+
+import "time"
+import bulkQuerygen "github.com/aryachanwu/BDC-TS/bulk_query_gen"
+
+// TimescaleVehicleLast produces Timescale-specific queries for the Vehicle single-host case.
+type TimescaleVehicleLast struct {
+	TimescaleVehicle
+}
+
+func NewTimescaleVehicleLast(dbConfig bulkQuerygen.DatabaseConfig, queriesFullRange bulkQuerygen.TimeInterval, queryInterval time.Duration, scaleVar int) bulkQuerygen.QueryGenerator {
+	underlying := newTimescaleVehicleCommon(dbConfig, queriesFullRange, queryInterval, scaleVar).(*TimescaleVehicle)
+	return &TimescaleVehicleLast{
+		TimescaleVehicle: *underlying,
+	}
+}
+
+func (d *TimescaleVehicleLast) Dispatch(i int) bulkQuerygen.Query {
+	q := NewSQLQuery() // from pool
+	d.LastValueOneHost(q)
+	return q
+}
