266b8fa519f24af26c42223adf1fce800d5c6b02
wuchan <wuchan@inspur.com>
2023-06-12 02:02:23 +0000

    add vehicle method

---
+++ bulk_query_gen/vehicle.go
@@ -0,0 +1,18 @@
+package bulk_query_gen
+
+// Devops describes a devops query generator.
+type Vehicle interface {
+	AvergeValueOneDayOneHost(Query)
+	AvergeValueSevenDaysOneHost(Query)
+	AvergeValueOneMonthOneHost(Query)
+	LastValueOneHost(Query)
+
+	//CountCPUUsageDayByHourAllHostsGroupbyHost(Query)
+
+	Dispatch(int) Query
+}
+
+// VehicleDispatchAll round-robins through the different devops queries.
+func VehicleDispatchAll(d Devops, iteration int, q Query, scaleVar int) {
+	d.AvergeValueOneDayOneHost(q)
+}
